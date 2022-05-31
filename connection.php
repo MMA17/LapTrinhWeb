e3bacca8e579a6e345641e1d3ab3198511cc607f
vietnh <vietnguyenhoang@vccorp.vn>
2022-05-31 16:43:16 +0700

    update version for nextpaas

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
     $server_username = "hoangviet";
     $server_password ="123456";
-    $server_host = "localhost";
+    $server_host = "103.69.194.222";
     $server_dbname = "ltweb";
 
     $conn = mysqli_connect($server_host,$server_username,$server_password,$server_dbname) or die ("Chưa kết nối được DataBase");
