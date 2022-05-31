cb47ae7e98deec90e585477e3e021e2add5c920a
vietnh <vietnguyenhoang@vccorp.vn>
2022-05-31 17:11:38 +0700

    update env

--- connection.php
+++ connection.php
@@ -1,9 +1,8 @@
 <?php
-    $server_username = "root";
-    $server_password ="";
-    $server_host = "localhost";
-    $server_dbname = "ltweb2";
+    $server_username = "hoangviet";
+    $server_password ="password";
+    $server_host = "103.69.194.221";
+    $server_dbname = "ltweb";
 
     $conn = mysqli_connect($server_host,$server_username,$server_password,$server_dbname) or die ("Chưa kết nối được DataBase");
-    mysqli_set_charset($conn, 'UTF8');
 ?>
